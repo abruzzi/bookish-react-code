248a9d47f9d8b11577ad7ddb063434b0033c52de
Juntao Qiu <juntao.qiu@gmail.com>
2018-02-12 10:13:09 +0800

    refactor the renderBooks

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  renderBooks(books) {
+    return (<div className="books">
+      {
+        books.map(book => {
+          return (<div className="book">
+            <h2 className="title">{book.name}</h2>
+          </div>)
+        })
+      }
+    </div>)
+  }
+
   render() {
+    const books = [{ name: 'Refactoring' }, { name: 'Domain-driven design' }]
     return (
       <div className="App">
         <h1>Bookish</h1>
-        <div className="books">
-          <div className="book">
-            <h2 className="title">Refactoring</h2>
-          </div>
-          <div className="book">
-            <h2 className="title">Domain-driven design</h2>
-          </div>
-        </div>
+        {this.renderBooks(books)}
       </div>
     );
   }
