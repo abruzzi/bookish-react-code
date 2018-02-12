fe6ee62e15984fa0d61077248e5f20edac4f8ea5
Juntao Qiu <juntao.qiu@gmail.com>
2018-02-12 11:34:14 +0800

    setup and teardown

--- src/e2e.test.js
+++ src/e2e.test.js
@@ -1,4 +1,5 @@
 import puppeteer from 'puppeteer'
+import axios from 'axios'
 
 const appUrlBase = 'http://localhost:3000'
 
@@ -11,6 +12,20 @@ beforeAll(async () => {
 })
 
 describe('Bookish', () => {
+  afterEach(() => {
+    return axios.delete('http://localhost:8080/books?_cleanup=true').catch(err => err)
+  })
+
+  beforeEach(() => {
+    const books = [
+      {"name": "Refactoring", "id": 1},
+      {"name": "Domain-driven design", "id": 2},
+      {"name": "Building Micro-service", "id": 3}
+    ]
+
+    return books.map(item => axios.post('http://localhost:8080/books', item, {headers: { 'Content-Type': 'application/json' }}))
+  })
+
   test('Heading', async () => {
     await page.goto(`${appUrlBase}/`)
     await page.waitForSelector('h1')
@@ -28,9 +43,10 @@ describe('Bookish', () => {
       return [...document.querySelectorAll('.book .title')].map(el => el.innerText)
     })
 
-    expect(books.length).toEqual(2)
+    expect(books.length).toEqual(3)
     expect(books[0]).toEqual('Refactoring')
     expect(books[1]).toEqual('Domain-driven design')
+    expect(books[2]).toEqual('Building Micro-service')
   })
 })
 
