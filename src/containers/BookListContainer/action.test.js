14222d082e1671b2fdcec6c65556eaf4549687e4
Juntao Qiu <juntao.qiu@gmail.com>
2018-02-14 11:31:00 +0800

    refactor types

--- src/containers/BookListContainer/action.test.js
+++ src/containers/BookListContainer/action.test.js
@@ -1,4 +1,5 @@
 import {setSearchTerm, fetchBooks} from './actions'
+import * as types from './types'
 
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
@@ -12,61 +13,58 @@ describe('BookListContainer related actions', () => {
   it('Set search keyword', () => {
     const term = ''
     const expected = {
-      type: 'SET_SEARCH_TERM',
+      type: types.SET_SEARCH_TERM,
       term
     }
     const action = setSearchTerm(term)
     expect(action).toEqual(expected)
   })
 
-  it('Fetch data successfully', () => {
-    const books = [
-      {id: 1, name: 'Refactoring'},
-      {id: 2, name: 'Domain-driven design'}
-    ]
-    axios.get = jest.fn().mockImplementation(() => Promise.resolve({data: books}))
+  describe('Fetching data from remote', () => {
+    it('Fetch data successfully', () => {
+      const books = [
+        {id: 1, name: 'Refactoring'},
+        {id: 2, name: 'Domain-driven design'}
+      ]
+      axios.get = jest.fn().mockImplementation(() => Promise.resolve({data: books}))
 
-    const expectedActions = [
-      { type: 'FETCH_BOOKS_PENDING'},
-      { type: 'FETCH_BOOKS_SUCCESS', payload: books }
-    ]
-    const store = mockStore({ books: [] })
+      const expectedActions = [
+        { type: types.FETCH_BOOKS_PENDING},
+        { type: types.FETCH_BOOKS_SUCCESS, payload: books }
+      ]
+      const store = mockStore({ books: [] })
 
-    return store.dispatch(fetchBooks()).then(() => {
-      expect(store.getActions()).toEqual(expectedActions)
+      return store.dispatch(fetchBooks()).then(() => {
+        expect(store.getActions()).toEqual(expectedActions)
+      })
     })
-  })
 
+    it('Search data with term in state', () => {
+      const books = [
+        {id: 1, name: 'Refactoring'},
+        {id: 2, name: 'Domain-driven design'}
+      ]
+      axios.get = jest.fn().mockImplementation(() => Promise.resolve({data: books}))
 
-  it('Search data with term in state', () => {
-    const books = [
-      {id: 1, name: 'Refactoring'},
-      {id: 2, name: 'Domain-driven design'}
-    ]
-    axios.get = jest.fn().mockImplementation(() => Promise.resolve({data: books}))
+      const store = mockStore({ books: [], term: 'domain' })
 
-    const expectedActions = [
-      { type: 'FETCH_BOOKS_PENDING'},
-      { type: 'FETCH_BOOKS_SUCCESS', payload: books }
-    ]
-    const store = mockStore({ books: [], term: 'domain' })
-
-    return store.dispatch(fetchBooks('')).then(() => {
-      expect(axios.get).toHaveBeenCalledWith('http://localhost:8080/books?q=domain')
+      return store.dispatch(fetchBooks('')).then(() => {
+        expect(axios.get).toHaveBeenCalledWith('http://localhost:8080/books?q=domain')
+      })
     })
-  })
 
-  it('Fetch data with error', () => {
-    axios.get = jest.fn().mockImplementation(() => Promise.reject({message: 'Something went wrong'}))
+    it('Fetch data with error', () => {
+      axios.get = jest.fn().mockImplementation(() => Promise.reject({message: 'Something went wrong'}))
 
-    const expectedActions = [
-      { type: 'FETCH_BOOKS_PENDING'},
-      { type: 'FETCH_BOOKS_FAILED', err: 'Something went wrong' }
-    ]
-    const store = mockStore({ books: [] })
+      const expectedActions = [
+        { type: types.FETCH_BOOKS_PENDING},
+        { type: types.FETCH_BOOKS_FAILED, err: 'Something went wrong' }
+      ]
+      const store = mockStore({ books: [] })
 
-    return store.dispatch(fetchBooks()).then(() => {
-      expect(store.getActions()).toEqual(expectedActions)
+      return store.dispatch(fetchBooks()).then(() => {
+        expect(store.getActions()).toEqual(expectedActions)
+      })
     })
   })
 })
--- src/containers/BookListContainer/actions.js
+++ src/containers/BookListContainer/actions.js
@@ -1,17 +1,18 @@
+import axios from 'axios'
+import * as types from './types'
+
 export const setSearchTerm = (term) => {
   return {type: 'SET_SEARCH_TERM', term}
 }
 
-import axios from 'axios'
-
 export const fetchBooks = () => {
   return (dispatch, getState) => {
-    dispatch({type: 'FETCH_BOOKS_PENDING'})
+    dispatch({type: types.FETCH_BOOKS_PENDING})
     const state = getState()
     return axios.get(`http://localhost:8080/books?q=${state.term}`).then((res) => {
-      dispatch({type: 'FETCH_BOOKS_SUCCESS', payload: res.data})
+      dispatch({type: types.FETCH_BOOKS_SUCCESS, payload: res.data})
     }).catch((err) => {
-      dispatch({type: 'FETCH_BOOKS_FAILED', err: err.message})
+      dispatch({type: types.FETCH_BOOKS_FAILED, err: err.message})
     })
   }
 }
