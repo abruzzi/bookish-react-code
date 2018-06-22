b64b9d7e2e75622e6ccc31c810d2f558f595e166
Juntao Qiu <juntao.qiu@gmail.com>
2018-06-22 14:10:46 +0800

    the review item

--- src/components/BookDetail/ReviewList/index.js
+++ src/components/BookDetail/ReviewList/index.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 class ReviewList extends  Component {
   render () {
+    const { reviews } = this.props
     return (<div className="reviews-container">
+      {reviews.map(review => <div className="review"></div>)}
     </div>)
   }
 }
--- src/components/BookDetail/ReviewList/index.test.js
+++ src/components/BookDetail/ReviewList/index.test.js
@@ -12,4 +12,16 @@ describe('ReviewList', () => {
     expect(wrapper.find('.reviews-container').length).toBe(1);
   })
 
+  it('Render List', () => {
+    const props = {
+      reviews: [
+        { name: 'Juntao', date: '2018/06/21', content: 'Excellent work, really impressive on the efforts you put'},
+        { name: 'Abruzzi', date: '2018/06/22', content: 'What a great book'}
+      ]
+    }
+
+    const wrapper = shallow(<ReviewList {...props}/>)
+    expect(wrapper.find('.reviews-container').length).toBe(1);
+    expect(wrapper.find('.review').length).toBe(2)
+  })
 })
