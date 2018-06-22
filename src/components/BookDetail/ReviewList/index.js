3d2bfbec0c8633e742d1ada38f4f49913e9af813
Juntao Qiu <juntao.qiu@gmail.com>
2018-06-22 22:10:36 +0800

    refactor review list a little bit

--- src/components/BookDetail/ReviewList/index.js
+++ src/components/BookDetail/ReviewList/index.js
@@ -3,16 +3,20 @@ import React, { Component } from 'react';
 import './index.css'
 
 class ReviewList extends  Component {
+  renderReview = (review) => {
+    return (<div className="review" key={`${review.name}-${review.date}`}>
+      <div>
+        <span className="name">{review.name}</span>
+        <span className="date">{review.date}</span>
+      </div>
+      <p>{review.content}</p>
+    </div>)
+  }
+
   render () {
     const { reviews } = this.props
     return (<div className="reviews-container">
-      {reviews.map(review => <div className="review" key={`${review.name}-${review.date}`}>
-        <div>
-          <span className="name">{review.name}</span>
-          <span className="date">{review.date}</span>
-        </div>
-        <p>{review.content}</p>
-      </div>)}
+      {reviews.map(this.renderReview)}
     </div>)
   }
 }
