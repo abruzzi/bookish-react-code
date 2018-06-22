cf3c05e692268fb99735c2f6606bec4fcc78cf61
Juntao Qiu <juntao.qiu@gmail.com>
2018-06-22 22:32:55 +0800

    review-list now is a presentence component

--- src/components/BookDetail/ReviewList/index.js
+++ src/components/BookDetail/ReviewList/index.js
@@ -4,13 +4,10 @@ import './index.css'
 
 import Review from './Review'
 
-class ReviewList extends  Component {
-  render () {
-    const { reviews } = this.props
-    return (<div className="reviews-container">
-      {reviews.map(review => <Review review={review} key={`${review.name}-${review.date}`} />)}
-    </div>)
-  }
+const ReviewList = ({reviews}) => {
+  return (<div className="reviews-container">
+    {reviews.map(review => <Review review={review} key={`${review.name}-${review.date}`} />)}
+  </div>)
 }
 
 export default ReviewList
