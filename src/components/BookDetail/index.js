fc887197ab4557dfd01d48ca4beecbd92418af7f
Juntao Qiu <juntao.qiu@gmail.com>
2018-06-22 21:40:04 +0800

    refactor the review list

--- src/components/BookDetail/index.js
+++ src/components/BookDetail/index.js
@@ -29,18 +29,31 @@ class BookDetail extends Component {
     })
   }
 
+  renderReviewList = () => {
+    const {book} = this.props
+
+    if(book.reviews) {
+      return <ReviewList reviews={book.reviews} />
+    }
+  }
+
+  renderReviewForm = () => {
+    return (<form className="review-form">
+      <input type="text" name="name" value={this.state.name} onChange={this.updateName} placeholder="Your name"/>
+      <textarea name="content" cols="30" rows="10" value={this.state.content} onChange={this.updateContent} placeholder="Write your review"/>
+      <button name="submit" onClick={this.saveReview}>Submit</button>
+    </form>)
+  }
+
   render() {
     const {book} = this.props
 
     return (<div className="detail">
       <h2 className="name">{book.name}</h2>
       <div className="description">{book.description ? book.description : book.name}</div>
-      <form className="review-form">
-        <input type="text" name="name" value={this.state.name} onChange={this.updateName} placeholder="Your name"/>
-        <textarea name="content" cols="30" rows="10" value={this.state.content} onChange={this.updateContent} placeholder="Write your review"/>
-        <button name="submit" onClick={this.saveReview}>Submit</button>
-      </form>
-      {book.reviews && <ReviewList reviews={book.reviews} />}
+
+      {this.renderReviewList()}
+      {this.renderReviewForm()}
     </div>)
   }
 }
