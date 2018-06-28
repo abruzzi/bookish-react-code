f74992d92679354e59446a0f7e3b233ab894ab91
Juntao Qiu <juntao.qiu@gmail.com>
2018-06-28 14:43:22 +0800

    fix warning for review list

--- src/components/BookDetail/ReviewList/Review/index.js
+++ src/components/BookDetail/ReviewList/Review/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 
-import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
 import { updateReview } from '../../../../containers/actions'
