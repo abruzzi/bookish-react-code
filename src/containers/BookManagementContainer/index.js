9c1d890f9f3b5d3c16d338b46f470a8085eff632
Juntao Qiu <juntao.qiu@gmail.com>
2019-05-25 16:24:53 +1000

    authors

--- src/containers/BookManagementContainer/index.js
+++ src/containers/BookManagementContainer/index.js
@@ -6,6 +6,10 @@ const BookManagementContainer = () => (<div>
     <h3>Basic information</h3>
     <input type="text" className="name" placeholder="Book Name"/>
     <input type="text" className="description" placeholder="Book Description"/>
+
+    <h3>Authors</h3>
+    <input type="text" className="author-name" placeholder="Author Name"/>
+    <input type="text" className="author-profile" placeholder="Author Profile"/>
   </form>
 </div>);
 
--- src/containers/BookManagementContainer/index.test.js
+++ src/containers/BookManagementContainer/index.test.js
@@ -6,8 +6,16 @@ describe('Book Management', () => {
   it('renders fields needed', () => {
     const wrapper = shallow(<BookManagementContainer/>);
 
-    expect(wrapper.find('h3').text()).toBe('Basic information');
+    expect(wrapper.find('h3').at(0).text()).toBe('Basic information');
     expect(wrapper.find('input[type="text"].name').exists()).toBe(true);
     expect(wrapper.find('input[type="text"].description').exists()).toBe(true);
+  })
+
+  it('renders authors', () => {
+    const wrapper = shallow(<BookManagementContainer/>);
+
+    expect(wrapper.find('h3').at(1).text()).toBe('Authors');
+    expect(wrapper.find('input[type="text"].author-name').exists()).toBe(true);
+    expect(wrapper.find('input[type="text"].author-profile').exists()).toBe(true);
   })
 })
